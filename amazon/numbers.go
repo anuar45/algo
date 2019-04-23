aa5971404989d31318797f910afddc993450c870
Anuar Serdaliyev <serdaliyev.anuar@gmail.com>
2019-04-23 23:07:34 +0200

    amaz numbers done

--- amazon/numbers.go
+++ amazon/numbers.go
@@ -1,31 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
 	fmt.Println("Numbers!")
 
-	//a := 57
+	a := 57
 	b := 548579342
 
-	fmt.Println(getSubInt(2, b))
-	fmt.Println(getIntLen(b))
+	fmt.Println(a, b)
+	//fmt.Println(getSubInt(6, 3, b))
+	//fmt.Println(getIntLen(b))
+	fmt.Println(getSubNumIndex(a, b))
 }
 
 func getSubNumIndex(a, b int) int {
 	aLen := getIntLen(a)
 	bLen := getIntLen(b)
-	var aIdx, bIdx int
-	for i := 0; i < aLen; i++ {
-		aIdx = getSubInt(i, a)
-		for j := 0; j < bLen; j++ {
-			bIdx = getSubInt(j, b)
-			if aIdx == bIdx {
-
-				break
-			}
+	for i := 0; i < bLen; i++ {
+		bSub := getSubInt(i, aLen, b)
+		if a == bSub {
+			return i
 		}
 	}
+	return 9999
 }
 
 func getIntLen(num int) int {
@@ -36,15 +37,15 @@ func getIntLen(num int) int {
 	return i
 }
 
-func getSubInt(index, num int) int {
+func getSubInt(index, length, num int) int {
 	var c, d, result int
 	numLen := getIntLen(num)
-	index = numLen - index - 1
+	index = numLen - index - length
 	t := 1
 	for i := 0; i <= index; i++ {
 		c = num / t
-		d = (num / (t * 10)) * 10
-		fmt.Println(c, d)
+		d = (num / (t * (int(math.Pow10(length))))) * (int(math.Pow10(length)))
+		//fmt.Println(c, d)
 		result = c - d
 		t *= 10
 	}
