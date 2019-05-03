c0fa012dffed702dc0584c2992487850e97b3911
Anuar Serdaliyev <serdaliyev.anuar@gmail.com>
2019-05-03 17:24:41 +0200

    longest substring in progress

--- longest-substring/main.go
+++ longest-substring/main.go
@@ -3,7 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("vim-go")
+	fmt.Println(lengthOfLongestSubstring("ub"))
+	fmt.Println(lengthOfLongestSubstring("abcdefghklljkfwkfjwklajewuifhkljf"))
+
+	fmt.Println(isStrCharsUnique("ub"))
+	fmt.Println(isStrCharsUnique("abcdefgh"))
+	fmt.Println(isStrCharsUnique("u"))
+	fmt.Println(isStrCharsUnique("ub"))
+	fmt.Println(isStrCharsUnique("abcdefghh"))
+	fmt.Println(isStrCharsUnique("abcddefgh"))
 }
 
 func lengthOfLongestSubstring(s string) int {
@@ -12,11 +20,16 @@ func lengthOfLongestSubstring(s string) int {
 	if len(s) == 1 {
 		return 1
 	}
+	if len(s) == 0 {
+		return 0
+	}
 
 	for i := 0; i < len(s); i++ {
 		for k := i; k < len(s); k++ {
-			if isStrCharsUnique(s[i:k]) && len(uniqueSubStr) < len(s[i:k]) {
+			if isStrCharsUnique(s[i:k]) && len(s[i:k]) > len(uniqueSubStr) {
+				fmt.Println(i, k, s[i:k])
 				uniqueSubStr = s[i:k]
+				fmt.Println(uniqueSubStr)
 			}
 		}
 	}
