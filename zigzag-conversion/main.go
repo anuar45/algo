be43edbe8519d7e82aea9501ece39566f984129c
Anuar Serdaliyev <serdaliyev.anuar@gmail.com>
2019-10-01 09:04:19 +0200

    some progress)

--- zigzag-conversion/main.go
+++ zigzag-conversion/main.go
@@ -11,5 +11,12 @@ func main() {
 func ZigZagCon(s string, n int) string {
 	var r string
 
+	for i := n; i >= 0; i-- {
+		for k := 0; k <= len(r); {
+			r += string(s[k])
+			k += i
+		}
+	}
+
 	return r
 }
