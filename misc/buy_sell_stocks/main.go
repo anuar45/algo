852e9e8c5e45d0939f3546101e85ffd559ff8a15
Anuar Serdaliyev <serdaliyev.anuar@gmail.com>
2020-05-08 21:35:56 +0200

    lc arrays: duplicates and buy sell

--- misc/buy_sell_stocks/main.go
+++ misc/buy_sell_stocks/main.go
@@ -8,6 +8,10 @@ func main() {
 	fmt.Println(maxProfit([]int{1, 2, 3, 4, 5}))
 	fmt.Println(maxProfit([]int{7, 6, 4, 3, 1}))
 
+	fmt.Println(maxProfitOptimized([]int{7, 1, 5, 3, 6, 4}))
+	fmt.Println(maxProfitOptimized([]int{1, 2, 3, 4, 5}))
+	fmt.Println(maxProfitOptimized([]int{7, 6, 4, 3, 1}))
+
 }
 
 func maxProfit(prices []int) int {
@@ -41,4 +45,14 @@ func maxProfit(prices []int) int {
 
 	}
 	return totalprofit
+}
+
+func maxProfitOptimized(prices []int) int {
+	var maxprofit int
+	for i := 1; i < len(prices); i++ {
+		if prices[i] > prices[i-1] {
+			maxprofit += prices[i] - prices[i-1]
+		}
+	}
+	return maxprofit
 }
