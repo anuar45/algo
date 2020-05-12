3ecd2bd88d4cf31f76952f36f74c77daedaa70a7
Anuar Serdaliyev <serdaliyev.anuar@gmail.com>
2020-05-12 22:34:51 +0200

    lc arrays: duplicates

--- misc/contains-duplicate/solution_test.go
+++ misc/contains-duplicate/solution_test.go
@@ -4,17 +4,26 @@ import "testing"
 
 func TestContainsDuplicate(t *testing.T) {
 	tc := []struct {
+		name  string
 		input []int
 		want  bool
 	}{
-		{[]int{1, 2, 3, 4, 5, 1}, true},
-		{[]int{1, 2, 3, 4, 5, 6}, false},
+		{"trueCase", []int{1, 2, 3, 4, 5, 1}, true},
+		{"falseCase", []int{1, 2, 3, 4, 5, 6}, false},
 	}
 
 	for _, tt := range tc {
-		got := containsDuplicate(tt.input)
-		if tt.want != got {
-			t.Errorf("want: %v\ngot: %v\n", tt.want, got)
-		}
+		t.Run(tt.name, func(t *testing.T) {
+			got := containsDuplicate(tt.input)
+			if tt.want != got {
+				t.Errorf("want: %v\ngot: %v\n", tt.want, got)
+			}
+		})
+	}
+}
+
+func BenchmarkContainsDuplicate(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		containsDuplicate([]int{1, 2, 3, 4, 5, 6, 8, 9})
 	}
 }
